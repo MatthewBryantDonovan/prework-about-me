fa075b22be3837447dedab27eeac4251aa4c37ed
Matthew Donovan <matthewbryantdonovan@gmail.com>
2019-10-01 22:47:06 -0400

    Pushing homework files

---
+++ PREWORK_MBD/Module-6/javascript.js
@@ -0,0 +1,19 @@
+function bigmode() {
+    document.getElementById("box").style.height = "300px";
+   }
+
+function bluemode() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
+function fademode() {
+    document.getElementById("box").style.opacity = "0.7";
+}
+
+function resetmode() {
+    document.getElementById("box").style = "height:150px; width:150px; background-color:orange; margin:25px";
+}
+
+function bordermode() {
+    document.getElementById("box").style.border = "25px ridge green";
+}
